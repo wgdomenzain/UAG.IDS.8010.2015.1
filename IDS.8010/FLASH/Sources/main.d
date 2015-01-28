c61b20080d6dec6b8a9cd6cc4ce07e6c4b58ae9a
joserparra <joseparraglez@gmail.com>
2015-01-27 20:29:21 -0600

    LCD PRINT NAME
    print name

--- IDS.8010/FLASH/Sources/main.d
+++ IDS.8010/FLASH/Sources/main.d
@@ -1,40 +1,40 @@
 Sources/main.o: ../Sources/main.c \
- C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
- C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
- C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
- C:/UAG.IDS.8010.2015.1/IDS.8010/Project_Headers/derivative.h \
- C:/UAG.IDS.8010.2015.1/IDS.8010/Project_Headers/MKL25Z4.h \
- C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
- C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
- C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
- C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
- C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
- C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
- C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
- C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h
+ C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ C:/Users/JoseR/Documents/GitHub/UAG.IDS.8010.2015.1/IDS.8010/Project_Headers/derivative.h \
+ C:/Users/JoseR/Documents/GitHub/UAG.IDS.8010.2015.1/IDS.8010/Project_Headers/MKL25Z4.h \
+ C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h
 
-C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
 
-C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
 
-C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
 
-C:/UAG.IDS.8010.2015.1/IDS.8010/Project_Headers/derivative.h:
+C:/Users/JoseR/Documents/GitHub/UAG.IDS.8010.2015.1/IDS.8010/Project_Headers/derivative.h:
 
-C:/UAG.IDS.8010.2015.1/IDS.8010/Project_Headers/MKL25Z4.h:
+C:/Users/JoseR/Documents/GitHub/UAG.IDS.8010.2015.1/IDS.8010/Project_Headers/MKL25Z4.h:
 
-C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
 
-C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
 
-C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
 
-C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
 
-C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
 
-C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
 
-C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
 
-C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
+C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
--- IDS.8010/FLASH/Sources/sa_mtb.d
+++ IDS.8010/FLASH/Sources/sa_mtb.d
@@ -1,10 +1,10 @@
 Sources/sa_mtb.o: ../Sources/sa_mtb.c \
- C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
- C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
- C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h
+ C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h
 
-C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
 
-C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
 
-C:/Freescale/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+C:/Users/JoseR/Documents/GDL/7cuatri/SOPE/CW\ for\ MCU\ 10.6/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
