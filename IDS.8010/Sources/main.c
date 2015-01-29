2f80ad3635c796fa3370d5ca39359627c682b038
MFerGtz <marifer1316@gmail.com>
2015-01-28 21:51:54 -0600

    Revert "LCD print name"
    This reverts commit 0792d9e711f937bf406d2f6b874c7a01e3529701.

--- IDS.8010/Sources/main.c
+++ IDS.8010/Sources/main.c
@@ -1,190 +1,26 @@
 /*
-@author: 		Fer Gtz
-@description: 	8020 Group
-@functions:		None
-@environment: 	KL25Z
-@date: 			22/01/2014
-@comments:		
-@version:		1.0 - Initial
-*/
+ * main implementation: use this 'C' sample to create your own application
+ *
+ */
+
+
+
+
 
 #include "derivative.h" /* include peripheral declarations */
 
 
-#define GPIO_PIN_MASK 0x1Fu
-#define GPIO_PIN(x) (((1)<<(x & GPIO_PIN_MASK)))
 
-#define	nBit0	0x01	//'00000001'
-#define	nBit1	0x02	//'00000010'
-#define	nBit2	0x04	//'00000100'
-#define	nBit3	0x08	//'00001000'
-#define	nBit4	0x10	//'00010000'
-#define	nBit5	0x20	//'00100000'
-#define	nBit6	0x40	//'01000000'
-#define	nBit7	0x80	//'10000000'
-
-//Time definitions
-#define nt15_msec	32000
-#define nt40_usec	800
-
-//LCD Control
-#define nIns	0
-#define nData	1
-
-#define PortLCD    	GPIOD_PDOR
-//Enable connected to portb_01
-#define Enable_1	GPIOB_PDOR |= 0x01
-#define Enable_0	GPIOB_PDOR &= 0xFE
-#define RS_1   		GPIOB_PDOR |= 0x02
-#define RS_0   		GPIOB_PDOR &= 0xFD
-
-
-
-
-#define	Set_GPIOB_PDOR(x)	(GPIOB_PDOR |= (1 << (x-1)))
-
-int int_Temp;
-
-//Cursor Blink off initialization
-const unsigned char InitializeLCD[5] = {0x38, 0x38, 0x38, 0x0C, 0x01};
-//--------------------------------------------------------------
-//Declare Prototypes
-/* Functions */
-void cfgPorts(void);
-void initLCD(void);
-void delay(long time);
-void sendCode(int Code, int Data);
-
-/*@description: Initial Port Cfg 
-*/
-			
 int main(void)
 {
 	int counter = 0;
-		
-	    //Configure ports
-	    cfgPorts();
-	    //Initialize LCD
-	    initLCD();
-	    //Set position to print character
-	    sendCode(nIns, 0x85);
-	    sendCode(nIns, 0x85);
-	    //Print character
-	    delay(nt40_usec);
-	    sendCode(nData, 'F');
-	    delay(nt40_usec);
-	    sendCode(nData, 'E');
-	    delay(nt40_usec);
-	    sendCode(nData, 'R');
-	    delay(nt40_usec);
-	    sendCode(nData, 'N');
-		delay(nt40_usec);
-	    sendCode(nData, 'A');
-	    delay(nt40_usec);
-	    sendCode(nData, 'N');
-	    delay(nt40_usec);
-	    sendCode(nData, 'D');
-	    delay(nt40_usec);
-	    sendCode(nData, 'A');
-	    sendCode(nIns, 0x85);	  
-	    for(;;) {	   
-	    	   	counter++;
-	    	}
+	
+	
+	
+	
+	for(;;) {	   
+	   	counter++;
+	}
+	
 	return 0;
-}
-
-void cfgPorts(void)
-{
-	//Turn on clock for portb
-	SIM_SCGC5 = SIM_SCGC5_PORTB_MASK;	
-	//Turn on clock for portd
-	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;	
-	////Turn on clock for portc
-	SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK;
-	
-	/* Set pins of PORTB as GPIO */
-	PORTB_PCR0= PORT_PCR_MUX(1);
-	PORTB_PCR1= PORT_PCR_MUX(1);
-	PORTB_PCR2=(0|PORT_PCR_MUX(1));
-	PORTB_PCR3=(0|PORT_PCR_MUX(1));
-	PORTB_PCR4=(0|PORT_PCR_MUX(1));
-	PORTB_PCR5=(0|PORT_PCR_MUX(1));
-	PORTB_PCR6=(0|PORT_PCR_MUX(1));
-	PORTB_PCR7=(0|PORT_PCR_MUX(1));
-	
-	
-	/* Set pins of PORTD as GPIO */
-	PORTD_PCR0= PORT_PCR_MUX(1);
-	PORTD_PCR1= PORT_PCR_MUX(1);
-	PORTD_PCR2=(0|PORT_PCR_MUX(1));
-	PORTD_PCR3=(0|PORT_PCR_MUX(1));
-	PORTD_PCR4=(0|PORT_PCR_MUX(1));
-	PORTD_PCR5=(0|PORT_PCR_MUX(1));
-	PORTD_PCR6=(0|PORT_PCR_MUX(1));
-	PORTD_PCR7=(0|PORT_PCR_MUX(1));
-	
-	//Initialize PortB
-	GPIOB_PDOR = 0x00;
-	
-	//Initialize PortD 
-	GPIOD_PDOR = 0x00;
-
-	//Configure PortB as outputs
-	GPIOB_PDDR = 0xFF;
-	
-	//Configure PortD as outputs
-	GPIOD_PDDR = 0xFF;
-	
-	//Configure PortC as inputs
-	GPIOC_PDDR = 0x00;
-}
-
-void initLCD(void)
-{
-	int i;
-	delay(nt15_msec);
-	
-	/* Send initialization instructions */
-	/* Loop for sending each character from the array */
-	for(i=0;i<5;i++)
-	{										
-		sendCode(nIns, InitializeLCD[i]);	/* send initialization instructions */			
-	}
-	
-}
-
-void sendCode(int Code, int Data)
-{
-	//Assign a value to pin RS
-	/*HINT: When RS is 1, then the LCD receives a data
-	when RS is 0, then the LCD receives an instruction */
-	// Initialize RS and Enable with 0
-	RS_0;
-	Enable_0;
-	//Assign the value we want to send to the LCD
-	PortLCD = Data;	
-	
-	//We make the algorithm to establish if its an instruction we start with 0 on RS value, otherwise if its a data command we start with RS as 1;
-	if (Code == nIns)
-	{
-		Enable_1;
-		delay(nt40_usec);
-		Enable_0;
-		RS_0;
-	}		
-	else if(Code == nData)
-	{
-		RS_1;
-		Enable_1;
-		delay(nt40_usec);
-		Enable_0;
-		RS_0;
-	}
-}
-void delay(long time)
-{
-	while (time > 0)
-	{
-		time--;
-	}
 }
