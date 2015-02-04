23bc476c799ca5fcf290966e3c5587a5f102ec67
caneloolivo <vicente.carlos@live.com.mx>
2015-02-04 00:28:29 -0600

    LCD Button_Timers
    LCD Button_Timers

--- IDS.8010/Sources/main.c
+++ IDS.8010/Sources/main.c
@@ -24,18 +24,22 @@
 #define nData   1
 //MACROS
 #define Port_LCD GPIOD_PDOR
- 
-int dife = 1;
-int canto = 0;
+ #define GPIO_PIN_MASK 0x1Fu
+#define GPIO_PIN(x) (((1)<<(x & GPIO_PIN_MASK)))
+int counter = 1;
+int decena = 0;
+int mili=0;
+int num=0;
+long valor = 0x30717;
 //Enable connected to portb_01
 #define Enable_1    GPIOB_PDOR |= 0x01
 #define Enable_0    GPIOB_PDOR &= 0xFE
 #define RS_1        GPIOB_PDOR |= 0x02
 #define RS_0        GPIOB_PDOR &= 0xFD
-#define ReadBitPortE(x) ((GPIOE_PDIR >> x) & 0x00000001)
+#define ReadBitPortB(x) ((GPIOB_PDIR >> x) & 0x00000001)
 #define ECLOCK ReadBitPortE(0)
 #define EDATA ReadBitPortE(1)
-void configurarPuertos(void);
+void cfgPorts(void);
 void initLCD(void);
 void delay(long time);
 void sendCode(int code, int data);
@@ -46,21 +50,102 @@ void iniTimer(void);
 void imprimeNum(int x, int y);
  
 int main(void) {
-    configurarPuertos();
+
+    //500ms 0x614350
+    //250ms  0x307175
+    //100ms 0x122870
+    //50ms 0x61434
+    //25ms 0x30717
+	int estado = 0;
+	cfgPorts();
     initLCD();
+    valor=0x30717;
     iniTimer();
     //cfgClock();
     //initRTC();
-    sendCode(nIns, 0x80);
-    sendCode(nIns, 0x80);
+    sendCode(nIns, 0x84);
+    sendCode(nIns, 0x84);
     for (;;) {
+    	if(ReadBitPortB(8) == 0){
+    		int x;
+    		for(x=0;x<150000;x++){    			
+    		}
+    		if(ReadBitPortB(8) == 0){
+    			estado++;
+    		if(estado==5)
+    		{
+    			estado=0;
+    		}
+    		switch(estado){
+    		case 0:
+    			mili=0;
+    			num=0;
+    			sendCode(nIns, 0x01);
+    			valor=0;
+    			iniTimer();
+    			counter = 0;
+    			decena = 0;
+    			valor = 0x30717;
+    			iniTimer();
+    			break;
+    		case 1:
+    			mili=0;
+    			num=1;
+    			sendCode(nIns, 0x01);
+    			valor=0;
+    			iniTimer();
+    			counter = 0;
+    			decena = 0;
+    			valor=0x61434;
+    			iniTimer();
+    			break;
+    		case 2:
+    			mili=0;
+    			num=2;
+    			sendCode(nIns, 0x01);
+    			valor=0;
+    			iniTimer();
+    			counter = 0;
+    			decena = 0;
+    			valor=0x122870;
+    			iniTimer();
+    			break;
+    		case 3:
+    			mili=0;
+    			num=3;
+    			sendCode(nIns, 0x01);
+    			valor=0;
+    			iniTimer();
+    			counter = 0;
+    			decena = 0;
+    			valor=0x307175;
+    			iniTimer();
+    			break;
+    		case 4:
+    			mili=0;
+    			num=4;
+    			sendCode(nIns, 0x01);
+    			valor=0;
+    			iniTimer();
+    			counter = 0;
+    			decena = 0;
+    			valor=0x614350;
+    			iniTimer();
+    			break;
+    		}
+    		}
+    	}
     }
  
     return 0;
 }
+void delay(long time) {
+	while (time > 0) {
+		time--;
+	}
+}
  
- 
-void configurarPuertos(void) {
+void cfgPorts(void) {
  
     //Turn on clock for portb
     SIM_SCGC5 = SIM_SCGC5_PORTB_MASK;
@@ -74,6 +159,7 @@ void configurarPuertos(void) {
     PORTB_PCR0 = PORT_PCR_MUX(1);
     PORTB_PCR1 = PORT_PCR_MUX(1);
     PORTB_PCR2 = PORT_PCR_MUX(1);
+    PORTB_PCR8 = PORT_PCR_MUX(1);
  
     //PORT E
     PORTE_PCR0 = PORT_PCR_MUX(1);
@@ -107,7 +193,7 @@ void configurarPuertos(void) {
  
     //Configure Port as outputs input 0, output 1  
     GPIOD_PDDR = 0xFFFF;
-    GPIOB_PDDR = 0xFF;
+    GPIOB_PDDR = 0xFFEFF;
     GPIOE_PDDR = 0xF0;
     GPIOC_PDDR = 0x00;
 }
@@ -121,11 +207,6 @@ void initLCD(void) {
     sendCode(nIns, 0x01);
 }
  
-void delay(long time) {
-    while (time > 0) {
-        time--;
-    }
-}
  
 void sendCode(int code, int data) {
     RS_0;
@@ -145,81 +226,16 @@ void sendCode(int code, int data) {
     }
 }
  
- 
-/*void cfgClock(void) {   //Configure 32 KHz output
-// Add cfg of Clock Source Select
-//Enable internal reference clock - page 372
-    MCG_C1 = MCG_C1_IRCLKEN_MASK;
-    //MCG_C1 |= MCG_C1 | MCG_C1_IRCLKEN_MASK;
- 
-    //Internal Reference Clock -->Slow - page 373
-    MCG_C2 &= ~(MCG_C2_IRCS_MASK);  //Internal Reference Clock -->Slow
- 
-    //Selects the 32 kHz clock source as RTC_CLKIN - page 194
-    SIM_SOPT1 = SIM_SOPT1_OSC32KSEL(2);
- 
-    // Selects the clock to output on the CLKOUT pin as MCGIRCLK (32 Khz) - page 196
-    SIM_SOPT2 |= SIM_SOPT2_CLKOUTSEL(4);
-}
- 
-void initRTC(void) {
-    //Enable the clock to RTC module register space - page 208
-    //Hint: Check if this register was initialized before
-    SIM_SCGC6 |= SIM_SCGC6_RTC_MASK;
- 
-    //Clear Registers
-    //First, clear all RTC registers - page 603
-    RTC_CR = RTC_CR_SWR_MASK;
-    //Clear SWR bit
-    //Send 0 to SWR bit
-    RTC_CR &= ~RTC_CR_SWR_MASK;
- 
-    if (RTC_SR_TIF_MASK) {
-        // This action clears the TIF. Remember:
-        // This bit is cleared by writing the TSR register when the time counter is disabled
-        // page 604
-        RTC_TSR = 0;
-    }
- 
-    // Set time compensation parameters (not strictly required) - page 600
-    RTC_TCR = RTC_TCR_CIR(1);
-    RTC_TCR |= RTC_TCR_TCR(255);
- 
-    // Enable RTC seconds irq - page 53 (remember modulus explanation)  
-    // Interrupt Clear-pending Register - Manual 2 page 112
-    NVIC_ICPR |= 1 << (21 % 32);
- 
-    // Interrupt Set-enable Register
-    // Interrupt Clear-pending Register - Manual 2 page 110
-    NVIC_ISER |= 1 << (21 % 32);
- 
-    // Enable Seconds Interrupt - page 606
-    RTC_IER |= RTC_IER_TSIE_MASK;
- 
-    // Time counter is enabled. This actions starts RTC counting - page 603
-    RTC_SR |= RTC_SR_TCE_MASK;
- 
-    // Configure the timer seconds and alarm registers - page 599
-    RTC_TSR = 0xFF;
- 
-}
- 
-void RTC_Seconds_IRQHandler(void) {
-    int cant = 0;
-    cant = count(dife);
-    sendCode(nIns, 0x80);
-    if(cant == 0){
-        sendCode(nData, '0');
-    } else {
-        imprimeNum(cant, dife);
-    }
-}*/
- 
 void iniTimer(void){
      // turn on PIT
     PIT_MCR = PIT_MCR_FRZ_MASK;    
     // Timer 1
-    PIT_LDVAL1 = 0x122870;
+    //500ms 0x614350
+    //250ms  0x307175
+    //100ms 0x122870
+    //50ms 0x61434
+    //25ms 0x30717
+    PIT_LDVAL1 = valor;
     PIT_TCTRL1 = PIT_TCTRL_TIE_MASK | PIT_TCTRL_TEN_MASK;
      
     NVIC_ICPR |= 1 << ((INT_PIT - 16) % 32);
@@ -228,32 +244,163 @@ void iniTimer(void){
 }
  
 void PIT_IRQHandler(void){
-	int cant = 0;
-    dife++;
-    if(dife == 10){
-    	dife = 0;
-    	canto++;
+    int cantDec = 0;
+    sendCode(nIns, 0x01);
+    switch(num){
+    case 0:
+    	mili=mili+25;
+    	if(mili==100)
+    	{
+    		mili=0;
+    		counter++;
+    		if(counter == 10){
+    			counter = 0;
+    		    decena++;
+    		}    		
+    	}    
+    	cantDec = count(decena);
+    	sendCode(nIns, 0x84);
+    	sendCode(nIns, 0x84);
+    	if(cantDec == 0){
+    		sendCode(nData, '0');
+    	} else {
+    		imprimeNum(cantDec, decena);
+    	 }
+    	sendCode(nData, '.');
+    	cantDec = count(counter);
+    	if(cantDec == 0){
+    		sendCode(nData, '0');
+    	} else {
+    		imprimeNum(cantDec, counter);
+    	}
+    	if(cantDec==0){
+    		sendCode(nData,'0');
+    	}
+    	else{
+    		imprimeNum(cantDec, mili);
+    	}
+    	PIT_TFLG1 |= PIT_TFLG_TIF_MASK;     // Clear the timer interrupt flag
+    	 
+    	PIT_TCTRL1 |= PIT_TCTRL_TEN_MASK | PIT_TCTRL_TIE_MASK;
+    	 
+    	break;
+    case 1:
+    	mili=mili+5;
+    	if(mili==10)
+    	{
+    		counter++;
+    		mili=0;
+    		if(counter == 10){
+    			counter = 0;
+    		    decena++;
+    		    }    		
+    	}    	
+    	cantDec = count(decena);
+    	sendCode(nIns, 0x84);
+    	sendCode(nIns, 0x84);
+    	if(cantDec == 0){
+    		sendCode(nData, '0');
+    	} else {
+    		imprimeNum(cantDec, decena);
+    	 }
+    	sendCode(nData, '.');
+    	cantDec = count(counter);
+    	if(cantDec == 0){
+    		sendCode(nData, '0');
+    	} else {
+    		imprimeNum(cantDec, counter);
+    	}
+    	if(cantDec==0){
+    		sendCode(nData,'0');
+    	}
+    	else{
+    		imprimeNum(cantDec, mili);
+    	}
+    	PIT_TFLG1 |= PIT_TFLG_TIF_MASK;     // Clear the timer interrupt flag
+    	 
+    	PIT_TCTRL1 |= PIT_TCTRL_TEN_MASK | PIT_TCTRL_TIE_MASK;
+    	break;
+    case 2:
+    	counter++;
+    	if(counter == 10){
+        	counter = 0;
+        	decena++;
+    	}
+    	cantDec = count(decena);
+    	sendCode(nIns, 0x84);
+    	sendCode(nIns, 0x84);
+    	if(cantDec == 0){
+        	sendCode(nData, '0');
+    	} else {
+    		imprimeNum(cantDec, decena);
+    	}
+    	sendCode(nData, '.');
+    	cantDec = count(counter);
+    	if(cantDec == 0){
+        	sendCode(nData, '0');
+    	} else {
+        	imprimeNum(cantDec, counter);
+    	}
+    	imprimeNum(cantDec,mili);
+    	PIT_TFLG1 |= PIT_TFLG_TIF_MASK;     // Clear the timer interrupt flag
+ 
+    	PIT_TCTRL1 |= PIT_TCTRL_TEN_MASK | PIT_TCTRL_TIE_MASK;
+    	break;
+    case 3:
+    	counter=counter+25;
+    	if(counter == 100){
+        	counter = 0;
+        	decena++;
+    	}
+    	cantDec = count(decena);
+    	sendCode(nIns, 0x84);
+    	sendCode(nIns, 0x84);
+    	if(cantDec == 0){
+        	sendCode(nData, '0');
+    	} else {
+    		imprimeNum(cantDec, decena);
+    	}
+    	sendCode(nData, '.');
+    	cantDec = count(counter);
+    	if(cantDec == 0){
+        	sendCode(nData, '0');
+    	} else {
+        	imprimeNum(cantDec, counter);
+    	}
+    	PIT_TFLG1 |= PIT_TFLG_TIF_MASK;     // Clear the timer interrupt flag
+ 
+    	PIT_TCTRL1 |= PIT_TCTRL_TEN_MASK | PIT_TCTRL_TIE_MASK;
+    	break;
+    case 4: 
+    	counter=counter+5;
+    	if(counter == 10){
+        	counter = 0;
+        	decena++;
+    	}
+    	cantDec = count(decena);
+    	sendCode(nIns, 0x84);
+    	sendCode(nIns, 0x84);
+    	if(cantDec == 0){
+        	sendCode(nData, '0');
+    	} else {
+    		imprimeNum(cantDec, decena);
+    	}
+    	sendCode(nData, '.');
+    	cantDec = count(counter);
+    	if(cantDec == 0){
+        	sendCode(nData, '0');
+    	} else {
+        	imprimeNum(cantDec, counter);
+    	}
+    	imprimeNum(cantDec,mili);
+    	PIT_TFLG1 |= PIT_TFLG_TIF_MASK;     // Clear the timer interrupt flag
+ 
+    	PIT_TCTRL1 |= PIT_TCTRL_TEN_MASK | PIT_TCTRL_TIE_MASK;
+    	break;
     }
-	cant = count(canto);
-	sendCode(nIns, 0x80);
-	if(cant == 0){
-		sendCode(nData, '0');
-	} else {
-		imprimeNum(cant, canto);
-	}
-	sendCode(nData, '.');
-	cant = count(dife);
-	if(cant == 0){
-		sendCode(nData, '0');
-	} else {
-		imprimeNum(cant, dife);
-	}
-	GPIOB_PTOR = 0x4;
-    PIT_TFLG1 |= PIT_TFLG_TIF_MASK;     // Clear the timer interrupt flag
- 
-    PIT_TCTRL1 |= PIT_TCTRL_TEN_MASK | PIT_TCTRL_TIE_MASK;
 }
- 
+
+//Obtener decenas 
 int count(int x){
     int res = 0;
      while (x > 0) {
@@ -272,6 +419,7 @@ void imprimeNum(int x, int y){
         numero[pos] = (char)(((int)'0')+y);
         sendCode(nData, numero[0]);
     } else {
+    	//Iterar para obtener digitos individualmente
         while(y > 0){
             digit = y % 10;
             numero[pos] = (char)(((int)'0')+digit);
