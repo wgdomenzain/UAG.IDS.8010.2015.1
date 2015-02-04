4b62f6677f5d6204ecc2392fd6ef1a01bddda6f6
avfernandezz <avfernandezz@gmail.com>
2015-02-04 14:24:21 -0600

    Timer con Push Button
    25, 50, 100,250,500 ms

--- IDS.8010/Sources/main.c
+++ IDS.8010/Sources/main.c
@@ -24,15 +24,18 @@
 #define nData   1
 //MACROS
 #define Port_LCD GPIOD_PDOR
- 
+ #define GPIO_PIN_MASK 0x1Fu
+#define GPIO_PIN(x) (((1)<<(x & GPIO_PIN_MASK)))
 int counter = 1;
+int estado = 0;
 int decena = 0;
+int valor = 0x30717;
 //Enable connected to portb_01
 #define Enable_1    GPIOB_PDOR |= 0x01
 #define Enable_0    GPIOB_PDOR &= 0xFE
 #define RS_1        GPIOB_PDOR |= 0x02
 #define RS_0        GPIOB_PDOR &= 0xFD
-#define ReadBitPortE(x) ((GPIOE_PDIR >> x) & 0x00000001)
+#define ReadBitPortB(x) ((GPIOB_PDIR >> x) & 0x00000001)
 #define ECLOCK ReadBitPortE(0)
 #define EDATA ReadBitPortE(1)
 void cfgPorts(void);
@@ -46,17 +49,64 @@ void iniTimer(void);
 void imprimeNum(int x, int y);
  
 int main(void) {
+
+    //500ms 0x614350
+    //250ms  0x307175
+    //100ms 0x122870
+    //50ms 0x61434
+    //25ms 0x30717
 	cfgPorts();
     initLCD();
     iniTimer();
+    //cfgClock();
+    //initRTC();
     sendCode(nIns, 0x84);
     sendCode(nIns, 0x84);
     for (;;) {
+    	if(ReadBitPortB(8) == 0){
+    		delay(1000000);
+    		estado++;
+    		if(estado > 4){
+    			estado = 0;
+    		}
+    		switch(estado){
+    		case 0:
+    			counter = 0;
+    			decena = 0;
+    			PIT_LDVAL1 = 0x30717;
+    			break;
+    		case 1:
+    			counter = 0;
+    			decena = 0;
+    			PIT_LDVAL1 = 0x61434;
+    			break;
+    		case 2:
+    			counter = 0;
+    			decena = 0;
+    			PIT_LDVAL1 = 0x122870;
+    			break;
+    		case 3:
+    			counter = 0;
+    			decena = 0;
+    			PIT_LDVAL1 = 0x307175;
+    			break;
+    		case 4:
+    			counter = 0;
+    			decena = 0;
+    			PIT_LDVAL1 = 0x614350;
+    			break;
+    		}
+    		
+    	}
     }
  
     return 0;
 }
- 
+void delay(long time) {
+	while (time > 0) {
+		time--;
+	}
+}
  
 void cfgPorts(void) {
  
@@ -72,6 +122,7 @@ void cfgPorts(void) {
     PORTB_PCR0 = PORT_PCR_MUX(1);
     PORTB_PCR1 = PORT_PCR_MUX(1);
     PORTB_PCR2 = PORT_PCR_MUX(1);
+    PORTB_PCR8 = PORT_PCR_MUX(1);
  
     //PORT E
     PORTE_PCR0 = PORT_PCR_MUX(1);
@@ -105,7 +156,7 @@ void cfgPorts(void) {
  
     //Configure Port as outputs input 0, output 1  
     GPIOD_PDDR = 0xFFFF;
-    GPIOB_PDDR = 0xFF;
+    GPIOB_PDDR = 0xFFEFF;
     GPIOE_PDDR = 0xF0;
     GPIOC_PDDR = 0x00;
 }
@@ -119,11 +170,6 @@ void initLCD(void) {
     sendCode(nIns, 0x01);
 }
  
-void delay(long time) {
-    while (time > 0) {
-        time--;
-    }
-}
  
 void sendCode(int code, int data) {
     RS_0;
@@ -147,7 +193,12 @@ void iniTimer(void){
      // turn on PIT
     PIT_MCR = PIT_MCR_FRZ_MASK;    
     // Timer 1
-    PIT_LDVAL1 = 0x122870;
+    //500ms 0x614350
+    //250ms  0x307175
+    //100ms 0x122870
+    //50ms 0x61434
+    //25ms 0x30717
+    PIT_LDVAL1 = 0x30717;
     PIT_TCTRL1 = PIT_TCTRL_TIE_MASK | PIT_TCTRL_TEN_MASK;
      
     NVIC_ICPR |= 1 << ((INT_PIT - 16) % 32);
@@ -156,12 +207,45 @@ void iniTimer(void){
 }
  
 void PIT_IRQHandler(void){
-    int cantDec = 0;
-    counter++;
-    if(counter == 10){
-        counter = 0;
-        decena++;
+    int cantDec = 0;    
+    switch(estado){
+    case 0:
+    	counter+=25;
+    	if(counter >= 1000){
+    		counter = 0;
+    		decena++;
+    	}
+    	break;
+    case 1:
+    	counter+=50;
+    	if(counter >= 1000){
+    		counter = 0;
+    		decena++;
+    	}
+    	break;
+    case 2: 
+    	counter+= 100;
+    	if(counter >= 1000){
+    	        counter = 0;
+    	        decena++;
+    	    }
+    	break;
+    case 3:
+    	counter+= 250;
+    	if(counter >= 1000){
+    	   counter = 0;
+    	   decena++;
+    	}
+    	break;
+    case 4: 
+    	counter+= 500;
+    	if(counter >= 1000){
+    		counter = 0;
+    	    decena++;
+    	    	    }
+    	break;
     }
+    
     cantDec = count(decena);
     sendCode(nIns, 0x84);
     if(cantDec == 0){
@@ -176,7 +260,6 @@ void PIT_IRQHandler(void){
     } else {
         imprimeNum(cantDec, counter);
     }
-    //Pin B2
     GPIOB_PTOR = 0x4;
     PIT_TFLG1 |= PIT_TFLG_TIF_MASK;     // Clear the timer interrupt flag
  
